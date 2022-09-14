4888c34581930edb7754a095948468975bf126a6
Madina <madinarn@code.edu.az>
2022-09-14 09:13:30 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var btn_add = document.getElementById('btn_add');
+btn_add.onclick = function () {
+    var value = document.getElementById('input').value
+    var h3 = document.createElement('h3');
+    var icon = document.createElement('i');
+    var div = document.createElement('div');
+    div.setAttribute('class', 'box')
+    icon.setAttribute('class', 'fa-solid fa-trash-can')
+    h3.innerHTML = value
+    var tdl = document.getElementById('tdl');
+    div.appendChild(h3)
+    div.appendChild(icon)
+    tdl.appendChild(div)
+    icon.onclick = () => {
+        div.remove();
+    }
+}
